689ae95358bd9dce8dbb28294674c2672e96cb22
andreivegs <andreive.giovanini.silva@gmail.com>
2016-06-28 17:55:13 -0500

    Modify checking sender on test file

--- Codes/Arduino Codes/test_ard_xbee_temp_addressed/test_ard_xbee_temp_addressed.ino
+++ Codes/Arduino Codes/test_ard_xbee_temp_addressed/test_ard_xbee_temp_addressed.ino
@@ -1,30 +1,38 @@
 
+#include<SoftwareSerial.h>
+
 const int debugLED = 13;
 int analogValue = 0, analogHigh, analogLow;
 byte discard;
 boolean received = false;
 float temp = 0;
+byte sender[4];
+
+SoftwareSerial xbee(2, 3);
 
 void setup() {
   pinMode(debugLED, OUTPUT);
   Serial.begin(9600);
+  xbee.begin(9600);
 }
 
 void loop() {
+  int i;
   // make sure everything we need is in the buffer
-  if (Serial.available() >= 21) {
+  if (xbee.available() >= 21) {
     // look for the start byte
-    if (Serial.read() == 0x7E) {
+    if (xbee.read() == 0x7E) {
       //blink debug LED to indicate when data is received
       digitalWrite(debugLED, HIGH);
       delay(10);
       digitalWrite(debugLED, LOW);
       // read the variables that we're not using out of the buffer
-      for (int i = 1; i < 19 ; i++) {
-        discard = Serial.read();
-      }
-      analogHigh = Serial.read();
-      analogLow = Serial.read();
+      for (i = 1; i < 8 ; i++) discard = xbee.read();
+      // address of sender (bytes from 8 to 11)
+      for (i = 0; i < 4; i++) sender[i] = xbee.read();
+      for (i = 12; i < 19; i++) discard = xbee.read();
+      analogHigh = xbee.read();
+      analogLow = xbee.read();
       analogValue = analogLow + (analogHigh * 256);
       temp = analogValue / 1023.0 * 1.23;
       temp = (temp - 0.5) / 0.01;
@@ -32,7 +40,9 @@ void loop() {
     }
   }
   if (received) {
-    Serial.print("Value received: ");
+    Serial.print("Sender address: ");
+    for(int i=0; i<4; i++) Serial.print(sender[i]);
+    Serial.print("\nValue received: ");
     Serial.print(analogValue);
     Serial.print(" = ");
     Serial.print(temp);
