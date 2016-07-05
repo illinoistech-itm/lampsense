71b90faa1ecb4e30bd0c823dd90f5f5899a18273
Breno Martins <brsomartins@gmail.com>
2016-07-05 12:21:28 -0500

    Change the HTML page layout using Bootstrap

--- Codes/Arduino Codes/test_ard_html_path/test_ard_html_path.ino
+++ Codes/Arduino Codes/test_ard_html_path/test_ard_html_path.ino
@@ -2,7 +2,7 @@
 #include <Ethernet.h>
 
 // Ethernet variables
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // MAC address from Ethernet shield sticker under board
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}; // MAC address from Ethernet shield sticker under board
 IPAddress ip(192, 168, 1, 252); // IP address, may need to change depending on network
 EthernetServer server(80); // create a server at port 80
 
@@ -78,13 +78,13 @@ void loop() {
 
 // switch LED and send back HTML for LED checkbox
 void processSelection(String httpReq) {
-  if (httpReq.indexOf("GET /command=left?") > -1) {
+  if (httpReq.indexOf("GET /?command=left") > -1) {
     left();
-  } else if (httpReq.indexOf("GET /command=right?") > -1) {
+  } else if (httpReq.indexOf("GET /?command=right") > -1) {
     right();
-  } else if (httpReq.indexOf("GET /command=on?") > -1) {
+  } else if (httpReq.indexOf("GET /?command=on") > -1) {
     on();
-  } else if (httpReq.indexOf("GET /command=off?") > -1) {
+  } else if (httpReq.indexOf("GET /?command=off") > -1) {
     off();
   } else {
     Serial.println("DEBUG");
@@ -92,28 +92,73 @@ void processSelection(String httpReq) {
 }
 
 void sendHtmlPage(EthernetClient client) {
-  client.println("<!DOCTYPE html>");
-  client.println("<html>");
-  client.println("<head>");
-  client.println("<title>Arduino Room Selection</title>");
-  client.println("</head>");
-  client.println("<body>");
-  client.println("<h1>Room Selection</h1>");
-  client.println("<p>Click to select the room.</p>");
-  client.println("<form method=\"get\" action=\"command=left\">");
-  client.println("<input type=\"button\" name=\"left\" value=\"Left\" onclick=\"submit();\">");
-  client.println("</form>");
-  client.println("<form method=\"get\" action=\"command=right\">");
-  client.println("<input type=\"button\" name=\"right\" value=\"Right\" onclick=\"submit();\">");
-  client.println("</form>");
-  client.println("<form method=\"get\" action=\"command=on\">");
-  client.println("<input type=\"button\" name=\"on\" value=\"All On\" onclick=\"submit();\">");
-  client.println("</form>");
-  client.println("<form method=\"get\" action=\"command=off\">");
-  client.println("<input type=\"button\" name=\"off\" value=\"All Off\" onclick=\"submit();\">");
-  client.println("</form>");
-  client.println("</body>");
-  client.println("</html>");
+  client.println(
+    "<!DOCTYPE html>"
+    "<html>"
+    " <head>"
+    "   <title></title>"
+    "   <meta charset=\"utf-8\">"
+    "   <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+    "   <!-- Latest compiled and minified CSS -->"
+    "   <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\" integrity=\"sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7\" crossorigin=\"anonymous\">"
+    "   <!-- Optional theme -->"
+    "   <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css\" integrity=\"sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r\" crossorigin=\"anonymous\">"
+    "   <script type=\"text/javascript\">"
+    "     function getUrlVars() {"
+    "       var vars = {};"
+    "       var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {"
+    "         vars[key] = value;"
+    "       });"
+    "       return vars;"
+    "     }"
+    "   </script>"
+    " </head>"
+    " <body>"
+    "   <div class=\"container\">"
+    "     <script type=\"text/javascript\">"
+    "       var command = getUrlVars()['command'];"
+    "       console.log(command);"
+    "       if (command != undefined) {"
+    "         document.write(`<div class=\"alert alert-info fade in\">"
+    "           <a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>`);"
+    "           if (command == 'left') {"
+    "             document.write('<strong>Processing...</strong> Lightning the path to the left.');"
+    "           } else if (command == 'right') {"
+    "             document.write('<strong>Processing...</strong> Lightning the path to the right.');"
+    "           } else if (command == 'on') {"
+    "             document.write('<strong>Processing...</strong> Turning all the lights on.');"
+    "           } else if (command == 'off') {"
+    "             document.write('<strong>Processing...</strong> Turning all the lights off.');"
+    "           }"
+    "         document.write('</div>');"
+    "       }"
+    "     </script>"
+    "     <div class=\"jumbotron\">"
+    "       <h1>Arduino + Philips Hue</h1> "
+    "       <p>Click on the buttons to operate the lights.</p> "
+    "     </div>");
+  client.println("     <div class=\"btn-group\">"
+    "       <a class=\"btn btn-primary btn-lg\" href=\"?command=left\">"
+    "         <span class=\"glyphicon glyphicon-arrow-left\" aria-hidden=\"true\"></span> Left"
+    "       </a>"
+    "       <a class=\"btn btn-primary btn-lg\" href=\"?command=right\">"
+    "         <span class=\"glyphicon glyphicon-arrow-right\" aria-hidden=\"true\"></span> Right"
+    "       </a>"
+    "       <a class=\"btn btn-primary btn-lg\" href=\"?command=on\">"
+    "         <span class=\"glyphicon glyphicon-lamp\" aria-hidden=\"true\"></span> Turn All On"
+    "       </a>"
+    "       <a class=\"btn btn-primary btn-lg\" href=\"?command=off\">"
+    "         <span class=\"glyphicon glyphicon-off\" aria-hidden=\"true\"></span> Turn All Off"
+    "       </a>"
+    "     </div>"
+    "   </div>"
+    "   <!-- Latest (compatible) compiled and minified jQuery -->"
+    "   <script src=\"https://code.jquery.com/jquery-2.2.4.min.js\" integrity=\"sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=\" crossorigin=\"anonymous\"></script>"
+    "   <!-- Latest compiled and minified JavaScript -->"
+    "   <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\" integrity=\"sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS\" crossorigin=\"anonymous\"></script>"
+    " </body>"
+    "</html>"
+  );
 }
 
 void left() {
