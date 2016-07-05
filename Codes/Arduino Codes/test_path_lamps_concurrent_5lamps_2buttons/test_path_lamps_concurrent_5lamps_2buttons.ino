008e32f84f3d20e9820b87a8f9428526581b883a
andreivegs <andreive.giovanini.silva@gmail.com>
2016-07-05 17:01:17 -0500

    Add test support for Xbess alongside lamps

--- Codes/Arduino Codes/test_path_lamps_concurrent_5lamps_2buttons/test_path_lamps_concurrent_5lamps_2buttons.ino
+++ Codes/Arduino Codes/test_path_lamps_concurrent_5lamps_2buttons/test_path_lamps_concurrent_5lamps_2buttons.ino
@@ -1,8 +1,10 @@
 #include <SPI.h>
 #include <Ethernet.h>
+#include <SoftwareSerial.h>
 
 //hue variables
-const char hueBridgeIP[] = "192.168.1.188"; // IP found for the Philips Hue bridge
+// const char hueBridgeIP[] = "192.168.1.188"; // IP found for the Philips Hue bridge
+const char hueBridgeIP[] = "10.101.2.54"; // IP found for the Philips Hue bridge
 const char hueUsername[] = "newdeveloper";  // Developer name created when registering a user
 const int hueBridgePort = 80;
 
@@ -18,6 +20,7 @@ int hueSat;
 int path[] = {0, 0, 0, 0, 0}; // -1: off, 0: on for normal lighting 1: left path; 2: right path; 3: left and right paths (sum)
 int pathLeft[] = {1, 1, 1, 0, 0};  // 1 for lamp being part of such path; 0 for not
 int pathRight[] = {1, 0, 0, 1, 1};
+int pathUsed[] = {0, 0};
 unsigned int color[numLamps]; // current color for each lamp
 String command;
 // String commandOn = "{\"on\": true,\"bri\": 215,\"effect\": \"colorloop\",\"alert\": \"select\",\"hue\": 0,\"sat\":0}"; // full command line
@@ -28,15 +31,19 @@ String commandRight = "{\"on\": true,\"bri\": 215,\"hue\": 50000,\"sat\":235}";
 const int debugLED = 13;
 int i, c;
 unsigned long int previousTime = 0xFFFFFFFF, currentTime;
+boolean showTemp=false;
 
 
 byte mac[] = {0x90, 0xA2, 0xDA, 0x0F, 0x49, 0x9A}; // Ethernet shield MAC address
-IPAddress ip(192, 168, 1, 201);                    // Arduino IP
+//IPAddress ip(192, 168, 1, 201);                    // Arduino IP
+IPAddress ip(10, 101, 2, 254);                    // Arduino IP
 
 // Initialize the Ethernet server library
 // with the IP address and port you want to use
 // (port 80 is default for HTTP):
 EthernetClient client;
+
+SoftwareSerial xbee(2, 3); // RX, TX
 
 void setup() {
 
@@ -44,10 +51,11 @@ void setup() {
   pinMode(button1, INPUT);
   pinMode(button2, INPUT);
   Serial.begin(9600);
+  xbee.begin(9600);
   Serial.println("Ethernet.begin initializing");
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
-    // try to congifure using IP address instead of DHCP:
+    // try to configure using IP address instead of DHCP:
     Ethernet.begin(mac, ip);
   }
   Serial.println("Ethernet.begin executado");
@@ -84,6 +92,9 @@ void loop() {
   // Evaluate if input was given via serial
   if (Serial.available() > 0) {
     c = Serial.read();
+    if (c =='t' || c=='T'){
+      getTemp();
+    }
     if (c == 'L' || c == 'l') {
       //getPreviousState();
       addPath(1, "Going to left <--");
@@ -104,6 +115,65 @@ void loop() {
 
 }
 
+
+void getTemp(){
+
+  byte discard, analogHigh, analogLow;
+  int analogValue = 0;
+  float temp = 0;
+  boolean received = false;
+  
+  // make sure everything we need is in the buffer
+  if (xbee.available() >= 21) {
+    // look for the start byte
+    if (xbee.read() == 0x7E) {
+      //blink debug LED to indicate when data is received
+      digitalWrite(debugLED, HIGH);
+      delay(10);
+      digitalWrite(debugLED, LOW);
+      // read the variables that we're not using out of the buffer
+      for (int i = 1; i < 19 ; i++) {
+        discard = xbee.read();
+      }
+      analogHigh = xbee.read();
+      analogLow = xbee.read();
+      analogValue = analogLow + (analogHigh * 256);
+      temp = analogValue / 1023.0 * 1.23;
+      temp = (temp - 0.5) / 0.01;
+      received = true;
+    }
+  }
+  if (received) {
+    Serial.print("Value received: ");
+    Serial.print(analogValue);
+    Serial.print(" = ");
+    Serial.print(temp);
+    Serial.println(" ºC");
+    tempToLamp(&temp);
+    delay(3000);
+    setAllLamps(0, commandOn);
+    pathUsed[0] = pathUsed[1] = 0;
+    received = false;
+  }
+  
+}
+
+void tempToLamp(float* temp) {
+
+  if (*temp < 15.0) {
+    command = "{\"on\": true,\"bri\": 215,\"hue\": 55000,\"sat\":235}";
+    setAllLamps(0, command);
+  }
+  else if (*temp < 25.0) {
+    command = "{\"on\": true,\"bri\": 215,\"hue\": 30000,\"sat\":235}";
+    setAllLamps(0, command);
+  }
+  else {
+    command = "{\"on\": true,\"bri\": 215,\"hue\": 5000,\"sat\":235}";
+    setAllLamps(0, command);
+  }
+
+}
 
 void setAllLamps(int numPath, String message) {  // numPath: -1 for all off, 0 for all on (normal), 1 for left, 2 for right
 
@@ -147,22 +217,30 @@ void addPath(int addedPath, String message) {  // addedPath: 1 for left, 2 for r
 
   Serial.println(message);
   if (addedPath == 1) { // left case
-    for (int j = 1; j < numLamps + 1; j++) {
-      if (pathLeft[j - 1] == 1) {
-        setHue(j, commandLeft);
-        path[j - 1] = path[j - 1] + addedPath;
-        color[j - 1] = 20000;
-        delay(50);
+    if (pathUsed[0] == 1) Serial.println("Error: left path is already being used");
+    else {
+      pathUsed[0] = 1;    // change - left path being used
+      for (int j = 1; j < numLamps + 1; j++) {
+        if (pathLeft[j - 1] == 1) {
+          setHue(j, commandLeft);
+          path[j - 1] = path[j - 1] + addedPath;
+          color[j - 1] = 20000;
+          delay(50);
+        }
       }
     }
   }
   else if (addedPath == 2) { // right case
-    for (int j = 1; j < numLamps + 1; j++) {
-      if (pathRight[j - 1] == 1) {
-        setHue(j, commandRight);
-        path[j - 1] = path[j - 1] + addedPath;
-        color[j - 1] = 50000;
-        delay(50);
+    if (pathUsed[1] == 1) Serial.println("Error: right path is already being used");
+    else {
+      pathUsed[1] = 1;    // change - right path being used
+      for (int j = 1; j < numLamps + 1; j++) {
+        if (pathRight[j - 1] == 1) {
+          setHue(j, commandRight);
+          path[j - 1] = path[j - 1] + addedPath;
+          color[j - 1] = 50000;
+          delay(50);
+        }
       }
     }
   }
@@ -174,22 +252,30 @@ void deletePath(int modifiedPath, String message) {  // modifiedPath: 1 for left
 
   Serial.println(message);
   if (modifiedPath == 1) { // left case
-    for (int j = 1; j < numLamps + 1; j++) {
-      if (pathLeft[j - 1] == 1) {
-        if (path[j - 1] == 3) setHue(j, commandRight);
-        else setHue(j, commandOn);
-        path[j - 1] = path[j - 1] - modifiedPath;
-        delay(50);
+    if (pathUsed[0] == 0) Serial.println("Error: left path is not being used");
+    else {
+      pathUsed[0] = 0;
+      for (int j = 1; j < numLamps + 1; j++) {
+        if (pathLeft[j - 1] == 1) {
+          if (path[j - 1] == 3) setHue(j, commandRight);
+          else setHue(j, commandOn);
+          path[j - 1] = path[j - 1] - modifiedPath;
+          delay(50);
+        }
       }
     }
   }
   else if (modifiedPath == 2) { // right case
-    for (int j = 1; j < numLamps + 1; j++) {
-      if (pathRight[j - 1] == 1) {
-        if (path[j - 1] == 3) setHue(j, commandLeft);
-        else setHue(j, commandOn);
-        path[j - 1] = path[j - 1] - modifiedPath;
-        delay(50);
+    if (pathUsed[1] == 0) Serial.println("Error: right path is not being used");
+    else {
+      pathUsed[1] = 0;
+      for (int j = 1; j < numLamps + 1; j++) {
+        if (pathRight[j - 1] == 1) {
+          if (path[j - 1] == 3) setHue(j, commandLeft);
+          else setHue(j, commandOn);
+          path[j - 1] = path[j - 1] - modifiedPath;
+          delay(50);
+        }
       }
     }
   }
@@ -227,7 +313,7 @@ boolean setHue(int lightNum, String command) {
 
 }
 
-boolean getHue(int lightNum) {
+/*boolean getHue(int lightNum) {
 
   if (client.connect(hueBridgeIP, hueBridgePort))
   {
@@ -267,9 +353,9 @@ boolean getHue(int lightNum) {
     Serial.println("getHue() - Command failed");
   return false;
 
-}
+}*/
 
-void getPreviousState() {
+/*void getPreviousState() {
 
   Serial.println("\n\nReading current state");
   getHue(1);
@@ -296,7 +382,7 @@ void getMessagesDebug(String message) {
   Serial.print(hueSat);
   Serial.println();
 
-}
+}*/
 
 
 
