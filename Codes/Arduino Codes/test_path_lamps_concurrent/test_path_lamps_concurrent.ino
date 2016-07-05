14c766bdfa82d19daa85b9988bafda3ba6188355
andreivegs <andreive.giovanini.silva@gmail.com>
2016-07-04 21:37:30 -0500

    Add test file to handle the situation in which 2 lamps are being used at the same time for different paths

--- Codes/Arduino Codes/test_path_lamps_concurrent/test_path_lamps_concurrent.ino
+++ Codes/Arduino Codes/test_path_lamps_concurrent/test_path_lamps_concurrent.ino
@@ -7,17 +7,22 @@ const char hueUsername[] = "newdeveloper";  // Developer name created when regis
 const int hueBridgePort = 80;
 
 const int pinButton = 7;
+const unsigned long int interval = 500; // 500 ms
 
 String hueOn;
 int hueBri;
 long hueHue;
 int hueSat;
+int path[] = {0, 0, 0}; // 0: used just for lighting; 1: left path; 2: right path; 3: both paths (sum)
+unsigned int color[3]; // current color for each lamp
 String command;
-// String commandOn = "{\"on\": true,\"bri\": 215,\"effect\": \"colorloop\",\"alert\": \"select\",\"hue\": 0,\"sat\":0}";
+// String commandOn = "{\"on\": true,\"bri\": 215,\"effect\": \"colorloop\",\"alert\": \"select\",\"hue\": 0,\"sat\":0}"; // full command line
 String commandOn = "{\"on\": true,\"bri\": 215,\"hue\": 0,\"sat\":0}";
 String commandOff = "{\"on\": false}";
 const int debugLED = 13;
 int i, c;
+unsigned long int previousTime = 0xFFFFFFFF, currentTime;
+
 
 byte mac[] = {0x90, 0xA2, 0xDA, 0x0F, 0x49, 0x9A}; // Ethernet shield MAC address
 IPAddress ip(192, 168, 1, 201);                    // Arduino IP
@@ -47,40 +52,58 @@ void setup() {
 
 void loop() {
 
+  // Evaluate state of lamps
+  for (i = 0; i < 3; i++) {
+    if (path[i] == 3) {
+      currentTime = millis();
+      if ((previousTime + interval) < currentTime) { // need to fix for millis reseting
+        color[i] = 70000 + color[i]; // if color is 20000 turn it to 50000 and vice versa
+        command = "{\"on\": true,\"bri\": 215,\"hue\": " + String(color[i]) + ",\"sat\":235}";
+        setHue(i + 1, command);
+        previousTime = currentTime;
+      }
+    }
+  }
+
+  // Evaluate if button was pressed
   if (digitalRead(pinButton) == HIGH) {
     command = "{\"on\": " + hueOn + ",\"bri\": " + hueBri + ",\"hue\": " + hueHue + ",\"sat\": " + hueSat + "}";
-    set3Lamps(command, 0, "\n\nRestauring previous state of lamp 3 for all lamps");
+    set3Lamps(command, 0, 0, "\n\nRestauring previous state of lamp 3 for all lamps");
   }
+
+  // Evaluate if input was given via serial
   if (Serial.available() > 0) {
     c = Serial.read();
     if (c == 'L' || c == 'l') {
       getPreviousState();
       command = "{\"on\": true,\"bri\": 215,\"hue\": 25178,\"sat\":235}";
-      set3Lamps(command, 2, "Going to left <--");
+      set3Lamps(command, 2, 1, "Going to left <--");
     }
     else if (c == 'R' || c == 'r') {
       getPreviousState();
       command = "{\"on\": true,\"bri\": 215,\"hue\": 50000,\"sat\":235}";
-      set3Lamps(command, 3, "Going to right -->");
+      set3Lamps(command, 3, 2, "Going to right -->");
     }
     else if (c == 'N' || c == 'n') {
       getPreviousState();
-      set3Lamps(commandOn, 0, "Turning all on"); // Second parameter to 0 for no difference between lamps
+      set3Lamps(commandOn, 0, 0, "Turning all on"); // Second parameter to 0 for no difference between lamps
     }
     else if (c == 'F' || c == 'f') {
       getPreviousState();
-      set3Lamps(commandOff, 0, "Turning all off");
+      set3Lamps(commandOff, 0, 0, "Turning all off");
     }
   }
 
 }
 
-void set3Lamps(String command, int lampOff, String message) {
+
+void set3Lamps(String command, int lampOff, int numPath, String message) {
 
   Serial.println(message);
   if (lampOff == 0) {
     for (int j = 1; j < 4; j++) {
       setHue(j, command);
+      path[j - 1] = 0;
       delay(50);
     }
   }
@@ -88,9 +111,11 @@ void set3Lamps(String command, int lampOff, String message) {
     for (int j = 1; j < 4; j++) {
       if (j == lampOff) {
         setHue(j, commandOff);
+        path[j - 1] = 0;
         delay(50);
       } else {
         setHue(j, command);
+        path[j - 1] = (path[j-1]+numPath)%4;
         delay(50);
       }
     }
