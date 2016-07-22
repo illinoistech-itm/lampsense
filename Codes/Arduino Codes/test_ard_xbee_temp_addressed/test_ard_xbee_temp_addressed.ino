aa97f39dac6abc03539a7ed457e48947654cc742
andreivegs <andreive.giovanini.silva@gmail.com>
2016-07-22 15:58:40 -0500

    Modify serial connection to Arduino Mega's Serial 1

--- Codes/Arduino Codes/test_ard_xbee_temp_addressed/test_ard_xbee_temp_addressed.ino
+++ Codes/Arduino Codes/test_ard_xbee_temp_addressed/test_ard_xbee_temp_addressed.ino
@@ -8,31 +8,31 @@ boolean received = false;
 float temp = 0;
 byte sender[4];
 
-SoftwareSerial xbee(12, 13);
+// SoftwareSerial xbee(12, 13);
 
 void setup() {
   pinMode(debugLED, OUTPUT);
   Serial.begin(9600);
-  xbee.begin(9600);
+  Serial1.begin(9600);
 }
 
 void loop() {
   int i;
   // make sure everything we need is in the buffer
-  if (xbee.available() >= 21) {
+  if (Serial1.available() >= 21) {
     // look for the start byte
-    if (xbee.read() == 0x7E) {
+    if (Serial1.read() == 0x7E) {
       //blink debug LED to indicate when data is received
       digitalWrite(debugLED, HIGH);
       delay(10);
       digitalWrite(debugLED, LOW);
       // read the variables that we're not using out of the buffer
-      for (i = 1; i < 8 ; i++) discard = xbee.read();
+      for (i = 1; i < 8 ; i++) discard = Serial1.read();
       // address of sender (bytes from 8 to 11)
-      for (i = 0; i < 4; i++) sender[i] = xbee.read();
-      for (i = 12; i < 19; i++) discard = xbee.read();
-      analogHigh = xbee.read();
-      analogLow = xbee.read();
+      for (i = 0; i < 4; i++) sender[i] = Serial1.read();
+      for (i = 12; i < 19; i++) discard = Serial1.read();
+      analogHigh = Serial1.read();
+      analogLow = Serial1.read();
       analogValue = analogLow + (analogHigh * 256);
       temp = analogValue / 1023.0 * 1.23;
       temp = (temp - 0.5) / 0.01;
